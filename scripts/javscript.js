3f8000be083ed14c4e01937eb20956229751e5dd
ibrar ul hassan <hassanibrar402@gmail.com>
2023-11-24 21:04:58 +0500

    initial push demo project

---
+++ scripts/javscript.js
@@ -0,0 +1,27 @@
+// var name_1 = document.getElementById("name")
+// var email = document.getElementById("email");
+// var company = document.getElementById("company");
+// var text_area = document.getElementById("last_name");
+// var text_area = document.getElementById("text_area");
+// var submit = document.getElementById("submit");
+
+// submit.onclick = function() 
+// {
+//   if(name_1.value != "" && email.value != "" && company.value != "" && last_name !== "" && text_area !== "")
+//  { 
+//      document.write("success") ;
+//  } else {
+//    document.write("Please Enter Correct Detail")
+//  }
+ 
+//  // {
+//  //     document.write("Success");
+//  // }
+// }
+  $(document).ready(function() {
+    $('#menuToggle').click(function() {
+      $('#menuItems').toggleClass('hidden');
+    });
+  });
+
+
